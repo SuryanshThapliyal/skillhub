66bd0a6e5bef58e10e31d1706d24ea72d6a0b5e5
suryanshthapliyal <thapliyalsuryansh@gmail.com>
2026-02-15 22:21:12 +0530

    test 2

--- server.js
+++ server.js
@@ -18,6 +18,10 @@ app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname)));
 
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
 let hasSeeded = false;
 let connectingPromise = null;
 
